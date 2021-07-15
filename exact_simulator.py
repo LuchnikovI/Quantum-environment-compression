c17936605761be86b0536f71c9699f19db609708
LuchnikovI <luchnikovilya@gmail.com>
2021-07-15 22:28:49 +0300

    Update exact_simulator.py

--- exact_simulator.py
+++ exact_simulator.py
@@ -66,7 +66,7 @@ def _mutual_inf(state):
     h2 = -jnp.log((rho2 * rho2.T).sum())
     state = state.reshape((4, 4))
     h12 = -jnp.log((state * state.T).sum())
-    return h1 + h2 - h12
+    return state.reshape((4, 4))
 
 
 class ExactFloquet:
