bc3a0528c080b081ba878aace426fc2cdd3b29b4
LuchnikovI <luchnikovilya@gmail.com>
2021-07-18 13:37:33 +0300

    Update environment_tuncation.py

--- environment_tuncation.py
+++ environment_tuncation.py
@@ -62,8 +62,7 @@ def embedding(gates,
             print(norm)
             if env[0].shape[0] > max_dim:
                 print('dim = {}'.format(env[0].shape[0]))
-    #embedding_matrices = environment.build_system(system_block, env)
-    return env#embedding_matrices
+    return environment.build_system(system_block, env)
 
 def dynamics_with_embedding(embedding_matrices,
                             in_state,
