7df36311d9fd8d1f93ae9f825c666013f1ff90a3
LuchnikovI <luchnikovilya@gmail.com>
2021-07-20 12:31:10 +0300

    Update exact_dynamics_simulation.py

--- exact_dynamics_simulation.py
+++ exact_dynamics_simulation.py
@@ -57,7 +57,7 @@ def choi(gates,
     in_state = jnp.concatenate([jnp.array([1.], dtype=dtype), jnp.zeros((2 ** n - 1,), dtype=dtype)], axis=0)
     state, _ = lax.scan(iter_over_in_state, in_state, state)
     state = jnp.tensordot(jnp.eye(4, dtype=dtype) / 2, state, axes=0)
-    state = state.reshape((2, -1))
+    state = state.reshape((-1,))
         
     first_layer = gates[::2]
     second_layer = gates[1::2]
